ec8d08fd4f857ba751897889d726a3c0a764e03c
phucvu460 <phucvu4697@gmail.com>
2021-05-22 03:37:20 +0700

    update

---
+++ src/Components/NavBar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import avatar from '../img/avatar.png';
+import {NavLink} from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <div className="NavBar">
+            <nav className="nav">
+                {/* <div className="profile">
+                    <img src={avatar} alt=""/>
+                 </div> */}
+
+                <ul className="nav-items">
+                    <li className="nav-item">
+                        <NavLink to="/" exact activeClassName="active">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/about" exact activeClassName="active">
+                            About Team
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/portfolios" exact activeClassName="active">
+                            Portfolios
+                        </NavLink>
+                    </li>
+
+                </ul>
+
+                <footer className="footer">
+                    <p>
+                        @2021 Lorem Ipsum
+                    </p>
+            </footer>
+            </nav>
+            
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/Components/allportfolios.js
@@ -0,0 +1,81 @@
+import vtca from '../img/vtca.gif';
+import voice from '../img/voice.png';
+import port3 from '../img/portImages/port2.jpg';
+import port4 from '../img/portImages/port3.jpg';
+import port6 from '../img/portImages/port5.jpg';
+import port7 from '../img/portImages/port6.jpg';
+
+const portfolios = [
+    {
+        id: 1,
+        category: 'Face Recognition',
+        link1: '#',
+        link2: '#',
+        icon1: '#',
+        icon2: '#',
+        image: vtca,
+        title: 'Face Recognition'
+    },
+    {
+        id: 2,
+        category: 'Speaker Recognition',
+        link1: '#',
+        link2: '#',
+        icon1: '#',
+        icon2: '#',
+        image: voice,
+        title: 'Speaker Recognition'
+    },
+    // {
+    //     id: 3,
+    //     category: 'Animation',
+    //     link1: 'www.youtube.com',
+    //     link2: 'www.github.com',
+    //     icon1: 'G',
+    //     icon2: 'Y',
+    //     image: port3,
+    //     title: 'Autodesk Maya'
+    // },
+    // {
+    //     id: 4,
+    //     category: 'Javascript',
+    //     link1: 'www.youtube.com',
+    //     link2: 'www.github.com',
+    //     icon1: 'G',
+    //     icon2: 'Y',
+    //     image: port4,
+    //     title: 'Vanilla Javascript Website'
+    // },
+    // {
+    //     id: 5,
+    //     category: 'React Js',
+    //     link1: 'www.youtube.com',
+    //     link2: 'www.github.com',
+    //     icon1: 'G',
+    //     icon2: 'Y',
+    //     image: port6,
+    //     title: 'Getting Started With React'
+    // },
+    // {
+    //     id: 6,
+    //     category: 'React Js',
+    //     link1: 'www.youtube.com',
+    //     link2: 'www.github.com',
+    //     icon1: 'G',
+    //     icon2: 'Y',
+    //     image: port7,
+    //     title: 'Vanilla Javascript Website'
+    // },
+    // {
+    //     id: 7,
+    //     category: 'Test Port',
+    //     link1: 'www.youtube.com',
+    //     link2: 'www.github.com',
+    //     icon1: 'G',
+    //     icon2: 'Y',
+    //     image: port4,
+    //     title: 'Vanilla Javascript Website'
+    // }
+]
+
+export default portfolios;
